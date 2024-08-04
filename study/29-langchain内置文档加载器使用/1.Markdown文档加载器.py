34e65f9969a35e690f7b4f6cceca526c9ecdbafa
xiaobo <zhaoboqi77@163.com>
2024-08-04 17:52:48 +0800

    markdown文件加载器分离元素

--- study/29-langchain内置文档加载器使用/1.Markdown文档加载器.py
+++ study/29-langchain内置文档加载器使用/1.Markdown文档加载器.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 
 # 加载时不分割，如果分割是按照元素类型分割，后续使用文本分割器进行分割
-loader = UnstructuredMarkdownLoader("./项目API资料.md")
+# loader = UnstructuredMarkdownLoader("./项目API资料.md")
+# documents = loader.load()
+#
+# print(documents)
+# print(len(documents))
+# print(documents[0].metadata)
+
+loader = UnstructuredMarkdownLoader("./项目API资料.md", mode="elements")
 documents = loader.load()
 
-print(documents)
-print(len(documents))
-print(documents[0].metadata)
+print(f"文档数量: {len(documents)}")
+for document in documents[:2]:
+    print(document)
